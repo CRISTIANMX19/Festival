05ebf77fdff10de9bce0b0fb59f318542b7548af
Cristian Renteria Olivares <crisolivares.contacto@gmail.com>
2023-11-09 23:39:43 -0600

    Se añadieron tareas para convertir las imagenes a webp y avif

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,58 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
 function css(callback){
     src('src/scss/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(dest('build/css'))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest('build/css'))
     
+    callback();
+}
+
+function imagenes(callback){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img/jpng'))
+
+    callback();
+}
+
+
+function formatoWebp(callback){
+    const opciones = {
+        quality: 50
+    };
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img/webp'))
+
+    callback();
+}
+
+function formatoAvif(callback){
+    const opciones = {
+        quality: 50
+    };
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img/avif'))
+
     callback();
 }
 
@@ -16,4 +61,8 @@ function dev(callback){
     callback();
 }
 
-exports.dev = dev;
+exports.css = css;
+exports.imagenes = imagenes
+exports.formatoWebp = formatoWebp;
+exports.formatoAvif = formatoAvif;
+exports.dev = parallel(imagenes, formatoWebp, formatoAvif, dev);
